4974dad47acf9d72ab8cdb311ff39276aedf0b13
Armin <arminsh610@gmail.com>
2024-06-21 01:32:13 +0330

    feat: add two task to asynq service

--- scheduler_application/go.mod
+++ scheduler_application/go.mod
@@ -13,11 +13,13 @@ require (
 	github.com/gdamore/encoding v1.0.1 // indirect
 	github.com/gdamore/tcell/v2 v2.7.4 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hibiken/asynq v0.24.1 // indirect
 	github.com/hibiken/asynq/tools v0.0.0-20240506061152-d04888e74845 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -25,6 +27,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
@@ -39,9 +42,16 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.19.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.15.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/term v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
--- scheduler_application/main.go
+++ scheduler_application/main.go
@@ -2,20 +2,147 @@ package main
 
 import (
 	"context"
-	"github.com/redis/go-redis/v9"
+	"fmt"
+	"github.com/hibiken/asynq"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
-func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "",
-		DB:       0,
-	})
+type Message struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Content   string             `bson:"content"`
+	SenderID  primitive.ObjectID `bson:"sender_id"`
+	RoomID    primitive.ObjectID `bson:"room_id"`
+	Timestamp time.Time          `bson:"timestamp"`
+}
 
-	if err := client.Ping(context.Background()).Err(); err != nil {
-		log.Fatalln("redis ping err:", err)
-	} else {
-		log.Println("redis ping success")
+const (
+	TypeGetOldMessages     = "get:old_messages"
+	TypeArchiveOldMessages = "archive:old_messages"
+)
+
+func initMongoClient() (*mongo.Collection, *mongo.Collection) {
+	opts := options.Client().ApplyURI("mongodb://mongodb:27017")
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Println("Connected to MongoDB!")
+	messagesCollection := client.Database("chat_app").Collection("messages")
+	articleCollection := client.Database("chat_app").Collection("archived_messages")
+	return messagesCollection, articleCollection
+}
+
+func handleGetOldMessagesTask(ctx context.Context, t *asynq.Task) error {
+	msgCollection, _ := initMongoClient()
+
+	coll := msgCollection
+	oneHourAgo := time.Now().Add(-1 * time.Hour)
+	filter := bson.M{"timestamp": bson.M{"$lt": oneHourAgo}}
+
+	cur, err := coll.Find(context.Background(), filter)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer cur.Close(context.Background())
+
+	var messages []Message
+	if err := cur.All(context.Background(), &messages); err != nil {
+		log.Println("oldsss", err)
+		return err
+	}
+
+	for _, msg := range messages {
+		log.Printf("Found old message: %v", msg)
+	}
+
+	return nil
+}
+
+func handleArchiveOldMessagesTask(ctx context.Context, t *asynq.Task) error {
+	coll, ar := initMongoClient()
+	archiveColl := ar
+	oneHourAgo := time.Now().Add(-1 * time.Hour)
+	filter := bson.M{"timestamp": bson.M{"$lt": oneHourAgo}}
+
+	cur, err := coll.Find(context.Background(), filter)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer cur.Close(context.Background())
+
+	var messages []Message
+	if err := cur.All(context.Background(), &messages); err != nil {
+		return err
+	}
+
+	log.Println(messages)
+
+	for _, msg := range messages {
+		log.Println(msg)
+		_, err := archiveColl.InsertOne(context.Background(), msg)
+		if err != nil {
+			log.Println("archive", err)
+			return err
+		}
+
+		//_, err = coll.DeleteOne(context.Background(), bson.M{"_id": msg.ID})
+		//if err != nil {
+		//	return err
+		//}
+		log.Printf("Archived message: %v", msg)
+	}
+
+	return nil
+}
+
+func main() {
+
+	srv := asynq.NewServer(
+		asynq.RedisClientOpt{Addr: "redis:6379"},
+		asynq.Config{
+			Concurrency: 10,
+		},
+	)
+
+	mux := asynq.NewServeMux()
+	mux.HandleFunc(TypeGetOldMessages, handleGetOldMessagesTask)
+	mux.HandleFunc(TypeArchiveOldMessages, handleArchiveOldMessagesTask)
+
+	go func() {
+		if err := srv.Run(mux); err != nil {
+			log.Fatalf("could not run asynq server: %v", err)
+		}
+	}()
+
+	scheduler := asynq.NewScheduler(
+		asynq.RedisClientOpt{Addr: "redis:6379"},
+		&asynq.SchedulerOpts{},
+	)
+
+	_, err := scheduler.Register("* * * * *", asynq.NewTask(TypeGetOldMessages, nil))
+	if err != nil {
+		log.Fatalf("could not register get old messages task: %v", err)
+	}
+
+	_, err = scheduler.Register("* * * * *", asynq.NewTask(TypeArchiveOldMessages, nil))
+	if err != nil {
+		log.Fatalf("could not register archive old messages task: %v", err)
+	}
+
+	if err := scheduler.Run(); err != nil {
+		log.Fatalf("could not run scheduler: %v", err)
+	}
+
+	select {}
+
 }
